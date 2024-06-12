a4383f9dd3442f9e9854fb5a42d1c5c6feb76e4b
Zene <oinkymail3@gmail.com>
2024-06-12 09:53:40 +0100

    some optimisation

--- src/Program.cs
+++ src/Program.cs
@@ -22,30 +22,52 @@ namespace cgl
             : base(width, height, title)
         {
             _size = (width / 2, height / 2);
+            //_size = (20, 20);
             
-            _map = new GLArray<bool>(_size);
-            _temp = new GLArray<bool>(_size);
+            _map = new GLArray<byte>(_size);
+            _temp = new GLArray<byte>(_size);
+            
+            _checkMap = new GLArray<bool>(_size);
+            _checkTemp = new GLArray<bool>(_size);
+            _ertfgyh = new GLArray<Colour>(_size);
             
             _texture = new Texture2D(TextureFormat.R8, TextureData.Byte);
-            //_texture.SetData<byte>(_size.X, _size.Y, BaseFormat.R, null);
+            _texture.SetData<byte>(_size.X, _size.Y, BaseFormat.R, null);
             _texture.SetData(_size.X, _size.Y, BaseFormat.R, _map);
             _texture.MagFilter = TextureSampling.Nearest;
             _texture.MinFilter = TextureSampling.Nearest;
             _texture.WrapStyle = WrapStyle.EdgeClamp;
             
+            _texture2 = new Texture2D(TextureFormat.Rgba8, TextureData.Byte);
+            _texture2.SetData<byte>(_size.X, _size.Y, BaseFormat.Rgba, null);
+            _texture2.SetData(_size.X, _size.Y, BaseFormat.R, _map);
+            _texture2.MagFilter = TextureSampling.Nearest;
+            _texture2.MinFilter = TextureSampling.Nearest;
+            _texture2.WrapStyle = WrapStyle.EdgeClamp;
+            
             _shad = new BoolShader();
+            
+            DrawContext.RenderState.Blending = true;
+            DrawContext.RenderState.SourceScaleBlending = BlendFunction.SourceAlpha;
+            DrawContext.RenderState.DestinationScaleBlending = BlendFunction.OneMinusSourceAlpha;
         }
         
         private BoolShader _shad;
         private Texture2D _texture;
+        private Texture2D _texture2;
         
-        private GLArray<bool> _map;
-        private GLArray<bool> _temp;
+        private GLArray<byte> _map;
+        private GLArray<byte> _temp;
+        
+        private GLArray<bool> _checkMap;
+        private GLArray<bool> _checkTemp;
+        private GLArray<Colour> _ertfgyh;
         private Vector2I _size;
         private bool _applied = false;
         private bool _palying  = false;
+        private bool _enter = false;
         
-        private double _scale = 2d;
+        private double _scale = 1d;
         private Vector2 _pan = 0d;
         private Vector2 _mp;
         
@@ -61,6 +83,12 @@ namespace cgl
             e.Context.Projection = Matrix4.CreateOrthographic(Width, Height, 0d, 1d);
             e.Context.View = Matrix4.CreateTranslation(_pan) * Matrix4.CreateScale(_scale);
             
+            if (_enter)
+            {
+                ApplyRules();
+                _enter = false;
+            }
+            
             if (!_palying) { goto Ignore; }
             
             if (!_applied && Division(0.1))
@@ -75,8 +103,18 @@ namespace cgl
             
             Ignore:
             _texture.EditData(0, 0, _size.X, _size.Y, BaseFormat.R, _map);
+            for (int x = 0; x < _size.X; x++)
+            {
+                for (int y = 0; y < _size.Y; y++)
+                {
+                    _ertfgyh[x, y] = _checkMap[x, y] ? new Colour(255, 0, 0, 100) : Colour.Zero;
+                }
+            }
+            _texture2.EditData(0, 0, _size.X, _size.Y, BaseFormat.Rgba, _ertfgyh);
             //_texture.SetData(_size.X, _size.Y, BaseFormat.R, _map);
             Draw(e.Context, new Box(0d, Size));
+            e.Context.Model = Matrix.Identity;
+            e.Context.DrawBox(new Box(0d, Size), _texture2);
         }
         
         private void Draw(IDrawingContext dc, IBox bounds)
@@ -97,7 +135,9 @@ namespace cgl
             pi.Y = _size.Y - pi.Y - 1;
             
             if (pi.X < 0 || pi.Y < 0 || pi.X >= _size.X || pi.Y >= _size.Y) { return; }
-            _map[pi.X, pi.Y] = !_map[pi.X, pi.Y];
+            _map[pi.X, pi.Y] = (byte)(1 - _map[pi.X, pi.Y]);
+            _checkMap[pi.X, pi.Y] = true;
+            WriteAround(_checkMap, pi.X, pi.Y);
         }
         protected override void OnMouseMove(MouseEventArgs e)
         {
@@ -114,6 +154,11 @@ namespace cgl
                 _palying = !_palying;
                 return;
             }
+            if (e[Keys.Enter])
+            {
+                _enter = true;
+                return;
+            }
         }
         protected override void OnScroll(ScrollEventArgs e)
         {
@@ -128,11 +173,14 @@ namespace cgl
                 double oldZoom = _scale;
                 _scale = newZoom;
                 
-                Vector2 v = (_mp - _pan);
-                Vector2 pointRelOld = v / oldZoom;
-                Vector2 pointRelNew = v / newZoom;
+                // Vector2 v = (_mp - _pan);
+                // Vector2 pointRelOld = v / oldZoom;
+                // Vector2 pointRelNew = v / newZoom;
 
-                _pan += (pointRelNew - pointRelOld) * newZoom;
+                // _pan += (pointRelNew - pointRelOld) * newZoom;
+                Vector2 pointRelOld = (_mp / oldZoom) - _pan;
+                Vector2 pointRelNew = (_mp / newZoom) - _pan;
+                _pan += pointRelNew - pointRelOld;
                 return;
             }
             if (this[Mods.Shift])
@@ -150,45 +198,83 @@ namespace cgl
             {
                 for (int y = 0; y < _size.Y; y++)
                 {
+                    //bool c = _checkMap[x, y];
+                    if (!_checkMap[x, y])
+                    {
+                        _temp[x, y] = _map[x, y];
+                        continue;
+                    }
+                    _checkMap[x, y] = false;
+                    
                     int n = CountNeighbours(x, y);
-                    bool alive = _map[x, y];
+                    bool alive = _map[x, y] == 1;
                     if (n == 3)
                     {
-                        _temp[x, y] = true;
+                        _temp[x, y] = 1;
+                        //_checkTemp[x, y] = true;
+                        if (!alive)
+                        {
+                            WriteAround(_checkTemp, x, y);
+                        }
                         continue;
                     }
                     if (!alive)
                     {
-                        _temp[x, y] = false;
+                        _temp[x, y] = 0;
                         continue;
                     }
                     if (n == 2)
                     {
-                        _temp[x, y] = true;
+                        _temp[x, y] = 1;
+                        //_checkTemp[x, y] = true;
                         continue;
                     }
-                    _temp[x, y] = false;
+                    _temp[x, y] = 0;
+                    WriteAround(_checkTemp, x, y);
                 }
             }
             
             // swap memory
-            GLArray<bool> gla = _map;
+            GLArray<byte> gla = _map;
             _map = _temp;
             _temp = gla;
+            //Array.Clear(_temp.Data, 0, _temp.Length);
+            
+            GLArray<bool> gla2 = _checkMap;
+            _checkMap = _checkTemp;
+            _checkTemp = gla2;
+            //Array.Clear(_checkTemp.Data, 0, _checkTemp.Length);
         }
         private int CountNeighbours(int x, int y)
-            => Convert.ToInt32(Get(x + 1, y)) + Convert.ToInt32(Get(x - 1, y)) +
-            Convert.ToInt32(Get(x + 1, y + 1)) + Convert.ToInt32(Get(x - 1, y + 1)) +
-            Convert.ToInt32(Get(x + 1, y - 1)) + Convert.ToInt32(Get(x - 1, y - 1)) +
-            Convert.ToInt32(Get(x, y + 1)) + Convert.ToInt32(Get(x, y - 1));
-        private bool Get(int x, int y)
+            => Get(x + 1, y) + Get(x - 1, y) +
+            Get(x + 1, y + 1) + Get(x - 1, y + 1) +
+            Get(x + 1, y - 1) + Get(x - 1, y - 1) +
+            Get(x, y + 1) + Get(x, y - 1);
+        private int Get(int x, int y)
         {
             if (x < 0 || y < 0 || x >= _size.X || y >= _size.Y)
             {
-                return false;
+                return 0;
             }
             
             return _map[x, y];
+        }
+        private static void Write(GLArray<bool> map, int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= map.Width || y >= map.Height) { return; }
+            
+            map[x, y] = true;
+        }
+        private static void WriteAround(GLArray<bool> map, int x, int y)
+        {
+            Write(map, x + 1, y);
+            Write(map, x - 1, y);
+            Write(map, x + 1, y + 1);
+            Write(map, x - 1, y + 1);
+            Write(map, x + 1, y - 1);
+            Write(map, x - 1, y - 1);
+            Write(map, x, y + 1);
+            Write(map, x, y - 1);
         }
     }
 }
